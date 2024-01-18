b3f01b8cb274fb1ad363fe5f1373b00c785fc45c
Daniella Vidanes <daniella.vidanes@siteminder.com>
2024-01-18 08:20:21 +0800

    added additional types

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -1,21 +1,24 @@
+type HairColor = 'blond' | 'n/a' | 'none' | 'brown' | 'brown, grey' | 'black' | 'auburn, white' | 'auburn, grey' | 'grey' | 'auburn' | 'white' | 'blonde'
+type SkinColor = 'fair' | 'gold' | 'white, blue' | 'white' | 'light' | 'white, red' | 'unknown' | 'green' | 'green-tan, brown' | 'pale' | 'metal' | 'dark' | 'none' | 'brown mottle' | 'brown' | 'grey' | 'mottled green' | 'orange' | 'blue, grey' | 'grey, red' | 'red' | 'blue' | 'grey, blue' | 'yellow' | 'tan' | 'fair, green, yellow' | 'silver, red' | 'green, grey' | 'red, blue, white' | 'brown, white'
+type EyeColor = 'blue' | 'yellow' | 'red' | 'brown' | 'blue-gray' | 'black' | 'hazel' | 'orange' | 'pink' | 'unknown' | 'red, blue' | 'gold' | 'green, yellow' | 'white'
 type Gender = 'male' | 'female' | 'n/a' | 'hermaphrodite' | 'none'
 
 export interface Character {
-  name: string 
-  height: string 
-  mass: string 
-  hair_color: string 
-  skin_color: string 
-  eye_color: string 
-  birth_year: string 
+  name: string
+  height: string
+  mass: string
+  hair_color: HairColor
+  skin_color: SkinColor
+  eye_color: EyeColor
+  birth_year: string
   gender: Gender
-  homeworld: string 
-  films: string[] 
-  species: string[] 
-  vehicles: string[] 
-  starships: string[] 
-  created: string 
-  edited: string 
+  homeworld: string
+  films: string[]
+  species: string[]
+  vehicles: string[]
+  starships: string[]
+  created: string
+  edited: string
   url: string
 }
 
